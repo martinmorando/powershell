607b30bdcd77c376a387201f44b0861c9200f90f
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-06-03 18:50:17 -0300

    add 002-navigation.ps1

---
+++ 002-navigation.ps1
@@ -0,0 +1,12 @@
+# Navigation
+
+# List files and directories in specified path
+Get-ChildItem -Path ".\Documents"
+
+# No path? Uses current working directory
+Get-ChildItem
+
+
+
+# Move to a different directory
+Set-Location -Path ".\Documents"
