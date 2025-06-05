8ec0ece7b4d753d937acef281a467b5a6c367209
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-06-05 17:04:48 -0300

    add 004-get-help.ps1

---
+++ 004-get-help.ps1
@@ -0,0 +1,10 @@
+<#
+Get-Help
+Short-description: Displays help about PowerShell cmdlets and concepts.
+#>
+
+# Generic help information
+Get-Help
+
+# Help information for the Get-Date cmdlet
+Get-Help Get-Date
