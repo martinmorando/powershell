88faf87a085903141a8983203b2193018a13f7ff
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-06-05 17:05:09 -0300

    add 005-get-command.ps1

---
+++ 005-get-command.ps1
@@ -0,0 +1,9 @@
+<#
+Get-Command
+Outputs all "commands that are installed on the computer, including cmdlets, aliases, 
+functions, filters, scripts, and applications."
+https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/get-command?view=powershell-7.5#description
+#>
+
+# "Without parameters, Get-Command gets all the cmdlets, functions, and aliases installed on the computer."
+Get-Command
